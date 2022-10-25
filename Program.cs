6866bdc71ae6d912f9914806adcf42f45ee226c6
RS31 <114557710+RadomirSz@users.noreply.github.com>
2022-10-25 19:54:55 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -22,3 +22,12 @@ if (i%5 == 0 || i%6 == 0 || i%11 == 0)
 {
     Console.WriteLine(i);
 }
+//zad5
+int suma = 0;
+int a = int.Parse(Console.ReadLine());
+for (int i = 0; i < a; i++)
+{
+    int x = int.Parse(Console.ReadLine());
+    suma += x;
+}
+Console.WriteLine(suma);
