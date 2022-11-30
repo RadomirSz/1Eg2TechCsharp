32f5d49e41b472b2c5a8d520d8028387538a32b0
RS31 <114557710+RadomirSz@users.noreply.github.com>
2022-11-30 09:35:38 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,45 +1,147 @@
-﻿//tablica - zmienna (kolekcja) do trzymania wielu zmiennych
-/*int a = 4;
-int b = 7;
-int d = 3;
-int c = 1;
-int e;*/
-/*int[] T = new int[4];
-T[0] = 1;
-T[1] = 6;
-T[2] = 4;
-T[3] = 7;
+﻿using System.Runtime.CompilerServices;
 
-Console.WriteLine(T[1]+ " " + T[3]);
-for (int i = 0; i < 4; i++)
+int n = 10;
+int p = 1;
+int k = 20;
+int[] T = new int[n];
+Random r = new Random();
+for (int i = 0; i < n; i++) 
 {
-    Console.WriteLine(T[i]);
-}*/
-/*
-int[] K = new int[] { 6, 4, 7, 2, 3, 1, 5, 9, 8 };
-for (int i = 0; i < K.Length; i++)
+    T[i] = r.Next(p, k);
+}
+for (int i = 0; i < n; i++)
 {
-    Console.Write($"{K[i]}");
-}*/
-/*int[] L = new int[6]{4, 5, 8, 9, 7, 6};
-int maks = L[0];
-for (int i = 0; i < L.Length; i++)
+    Console.Write(T[i] + " ");
+}
+Console.WriteLine();
+//zad1 najwieksza
+/*int maks = T[0];
+for (int i = 0; i < n; i++)
 {
-    if (L[i] > maks)
-    {
-        maks = L[i];
-    }
+    if (maks < T[i])
+        {
+        maks = T[i];
+        }
 }
 Console.WriteLine(maks);
 */
-//znajdź mini wartość w tablicy (6,2,1,4,9,5)
-int[]G = new int[6] {6,2,1,4,9,5};
-int mini = int.MaxValue;
-for (int i = 0; i < G.Length; i++)
+//zad2
+/*int mini = T[0];
+for (int i = 0; i < n; i++)
 {
-    if (G[i] < mini)
+    if (mini < T[i])
     {
-        mini = G[i];
+        mini = T[i];
     }
 }
 Console.WriteLine(mini);
+*/
+//zad3
+
+//zad5
+/*int mini = T[0];
+int maks = T[0];
+for (int i = 0; i < n; i++)
+{
+    if (maks < T[i])
+    {
+        maks = T[i];
+    }
+    if (mini > T[i])
+    {
+        mini = T[i];
+    }
+}
+Console.Write("różnica: ");
+Console.Write(maks - mini);*/
+//zad6
+/*int suma = 0;
+for (int i = 0; i < n; i++)
+{
+    suma = suma + T[i];
+}
+Console.WriteLine(suma);
+*/
+//zad7
+/*double suma = 0;
+for (int i = 0; i < n; i++)
+{
+    suma = suma + T[i];
+}
+Console.WriteLine(suma);
+Console.WriteLine(suma/n);
+*/
+//zad8
+/*int pa = 0;
+int np = 0;
+for (int i = 0; i < n; i++)
+{
+    if (T[i] % 2 == 0)
+    { pa = pa + 1; }
+    else
+    { np = np + 1; }
+}
+Console.WriteLine(pa);
+Console.WriteLine(np);*/
+//ZAD9 źle
+/*int pierwsze = 0;
+int suma = 0;
+for (int i = 0; i < n; i++)
+{
+    for (int j = 1; j < T[i]; j++)
+    {
+        if (T[i] % j == 0)
+        {
+            suma = suma + 1;
+        }
+    }
+    if (suma > 2)
+    {
+        pierwsze = pierwsze + 1;
+    }
+    suma = 0;
+}
+Console.WriteLine(pierwsze);*/
+
+
+
+//zad10 też źle
+int maks = p;
+for (int i = 0; i < n; i++)
+{
+    if (maks < T[i])
+    {
+        maks = T[i];
+    }
+}
+Console.WriteLine(maks);
+
+int mini = p;
+for (int i = 0; i < n; i++)
+{
+    if (mini < T[i])
+    {
+        mini = T[i];
+    }
+}
+Console.WriteLine(mini);
+
+int vmaks = p;
+for (int i = 0; i < n; i++)
+{
+    if (vmaks < T[i] && T[i] < maks)
+    {
+        vmaks = T[i];
+    }
+}
+Console.WriteLine(vmaks);
+
+int vmini = p;
+for (int i = 0; i < n; i++)
+{
+    if (vmini < T[i] && T[i]>mini)
+    {
+        vmini = T[i];
+    }
+}
+Console.WriteLine(vmini);
