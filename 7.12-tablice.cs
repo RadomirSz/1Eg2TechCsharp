3b240115e1048b6fa60a261231311bc27642dd00
RS31 <114557710+RadomirSz@users.noreply.github.com>
2022-12-07 08:29:20 +0100

    lekcja 7.12 tablice 2 wymiarowe

---
+++ 7.12-tablice.cs
@@ -0,0 +1,20 @@
+﻿int sumap = 0;
+int suman = 0;
+int[] T = new int[100];
+Random r = new Random();
+for (int i = 0; i < T.Length; i++)
+{
+    T[i] = r.Next(10, 100);
+}
+for (int i = 0; i < T.Length; i++)
+{
+    if (T[i] % 2 == 0)
+    {
+        sumap = sumap + T[i];
+    }
+    else
+    {
+        suman = suman + T[i];
+    }    
+}
+Console.WriteLine(sumap +" "+suman);
